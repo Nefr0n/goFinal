bf1cf0afc50c7b7336efd17a65e9075f875c6a6a
Amir Assylbekov <121290249+Nefr0n@users.noreply.github.com>
2025-04-09 20:06:11 +0500

    Add files via upload

---
+++ awesomeProject/goFinal.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("List of ToDos")
+	fmt.Println("Welcome to ToDoList program")
+
+	fmt.Println("Self-study")
+	fmt.Println("Go final project")
+
+	fmt.Println("Body")
+	fmt.Println("MondayLegs")
+	fmt.Println("TuesdayLegs")
+	fmt.Println("SundayPool")
+}
